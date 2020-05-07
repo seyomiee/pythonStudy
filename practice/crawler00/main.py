8e07f1680846a58f6af0a7d327a39a00324ad3bc
seyeon <tltptltptltp@gmail.com>
2020-05-07 23:02:10 +0900

    siteiru

--- practice/crawler00/main.py
+++ practice/crawler00/main.py
@@ -2,7 +2,27 @@ from flask import Flask, render_template, request, Response
 import requests
 from bs4 import BeautifulSoup
 
-app = Flask('jobs')
+'''
+https://ranking.rakuten.co.jp/daily/gender={gender}/p={page}/
+'''
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+}
+
+def rank():
+  r= requests.get("https://ranking.rakuten.co.jp/daily/gender=female/p=1/",headers=headers)
+  soup= BeautifulSoup(r.text,'html5lib')
+  pager= soup.find('div',{'class':'pager'})
+  pages= pager.find_all('a')
+  num_page=[]
+  for a in pages:
+    num_page.append(a.find('span').string)
+  print(num_page[:-2])
+
+rank()
+
+app = Flask('rankings')
 
 @app.route('/')
 def home():
