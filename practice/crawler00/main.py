37496356ad348cd0831c6a97842603756cc8f437
seyeon <tltptltptltp@gmail.com>
2020-05-08 22:36:05 +0900

    home

--- practice/crawler00/main.py
+++ practice/crawler00/main.py
@@ -14,11 +14,10 @@ def rank():
   r= requests.get("https://ranking.rakuten.co.jp/daily/gender=female/p=1/",headers=headers)
   soup= BeautifulSoup(r.text,'html5lib')
   pager= soup.find('div',{'class':'pager'})
-  pages= pager.find_all('a')
+  pages= pager.find_all('span',{'class':'pageDisp'})
   num_page=[]
   for a in pages:
-    num_page.append(a.find('span').string)
-  print(num_page[:-2])
+    num_page.append(a.string)
 
 rank()
 
@@ -27,5 +26,15 @@ app = Flask('rankings')
 @app.route('/')
 def home():
   return render_template('home.html')
+
+@app.route('/result')
+def result():
+  for item in gender:
+    g=request.args.get(item)
+    print(g)
+    #a= request.args.get(age)
+    #if g:
+    
+  return render_template('result.html',g=g)
 
 app.run(host="0.0.0.0", debug=True)
