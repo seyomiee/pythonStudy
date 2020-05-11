9ca3e3ea36a37e416fc5284969701942dc7b837c
seyeon <tltptltptltp@gmail.com>
2020-05-11 23:27:04 +0900

    연습

--- practice/crawler00/main.py
+++ practice/crawler00/main.py
@@ -3,32 +3,65 @@ import requests
 from bs4 import BeautifulSoup
 
 '''
-https://ranking.rakuten.co.jp/daily/gender={gender}/p={page}/
+https://ranking.rakuten.co.jp/weekly/gender={gender}/p={page}/
 '''
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
 }
+'''
+gender ={
+  'femal','male'
+}
+'''
+
+app = Flask('rank')
+
+ranking=[]
+
+def search_ranking(rank_list):
+  for item in rank_list:
+      name= item.find('div',{'class': 'rnkRanking_itemName' }).string
+      print('⛱')
+      print(name)
 
 def rank():
-  r= requests.get("https://ranking.rakuten.co.jp/daily/gender=female/p=1/",headers=headers)
-  soup= BeautifulSoup(r.text,'html5lib')
-  pager= soup.find('div',{'class':'pager'})
-  pages= pager.find_all('span',{'class':'pageDisp'})
-  num_page=[]
-  for a in pages:
-    num_page.append(a.string)
-    #page 수 만큼 for문,,
-  for i,index in enumerate(num_page[:-2]):
-    print(i, index)
+  req= requests.get("https://ranking.rakuten.co.jp/daily/gender=female/p=1/",headers=headers)
+  if req:
+    soup= BeautifulSoup(req.text,'html5lib')
+    main= soup.find_all('div',{'class':'rnkRankingMain'})
+    '''
+    pager= soup.find('div',{'class':'pager'})
+    pages= pager.find_all('span',{'class':'pageDisp'})
+    
+    num_page=[]
+    for a in pages:
+      num_page.append(a.string)
+      #page 수 만큼 for문,,
+    
+    for i,index in enumerate(num_page[:-2]):
+      print(i, index)
+    '''
+    #1,2,3위
+    #4위 이하
+    #ranking_before=soup.find_all('div', {'class':'rnkRanking_top3box'})
+    if main:
+      ranking_after= main.find_all('div', {'class':'rnkRanking_after4box'})
+      print(ranking_after)
+      #search_ranking(ranking_before)
+      #search_ranking(ranking_after)
 
-rank()
 
-app = Flask('rankings')
+
+  
+
+
+
+
 '''
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('home.html',age=age,gender=gender)
 
 @app.route('/result')
 def result():
@@ -39,5 +72,10 @@ def result():
     #if g:
     
   return render_template('result.html',g=g)
+
 '''
-app.run(host="0.0.0.0", debug=True)
+
+rank()
+
+app.run(host="0.0.0.0")
+
