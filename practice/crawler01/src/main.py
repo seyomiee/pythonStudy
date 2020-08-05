5c6124eaa917d0ff5f6359a2de2bd46b52a655d6
seyeon <tltptltptltp@gmail.com>
2020-08-05 23:41:19 +0900

    다시공부,,

--- practice/crawler01/src/main.py
+++ practice/crawler01/src/main.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, request, Response
-from bs4 import BeautifulSoup
+#import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-app = Flask()
+url="https://www.youtube.com/channel/UCYaBl0gtXV_3sHW4bQjq0hA/videos"
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+elements = driver.find_elements(video-title,'a')
+for e in elements:
+  print(e.text)
 
 
-app.run(host="0.0.0.0", debug=True)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+}
+
+@app.route('/')
+def home():
+  return hello;
+
+app.run(host="0.0.0.0",debug=TRUE)
