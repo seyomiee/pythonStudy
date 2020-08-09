c95a7d8103d6fccbfe282ba56466b33ca770dbf6
seyeon <tltptltptltp@gmail.com>
2020-08-09 23:41:48 +0900

    .

--- practice/crawler01/src/main.py
+++ practice/crawler01/src/main.py
@@ -22,13 +22,8 @@ for e in elements:
   print(e.text)
 
 
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
-}
-
 @app.route('/')
 def home():
-  return hello;
+  return
 
 app.run(host="0.0.0.0",debug=TRUE)
